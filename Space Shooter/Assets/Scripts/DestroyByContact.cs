6f6b3a057a5cfdd42a4c004f865e5bca78d30406
insidexml <shusiming@gmail.com>
2015-03-23 19:42:49 -0700

    Asteroids!

---
+++ Space Shooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Boundary") {	//to make sure that it doesn't run into the boundary
+			return;
+		}
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+	}
+}
