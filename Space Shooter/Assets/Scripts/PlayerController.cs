2eac998c8ed5477caf5434cae193f167e3956059
insidexml <shusiming@gmail.com>
2015-03-24 19:53:07 -0700

    Audio!
    Background music, sound when firing, sound when asteroids are destroyed,
    sound when player is destroyed.

--- Space Shooter/Assets/Scripts/PlayerController.cs
+++ Space Shooter/Assets/Scripts/PlayerController.cs
@@ -22,8 +22,8 @@ public class PlayerController : MonoBehaviour {
 	void Update(){
 		if (Input.GetButton("Fire1") && Time.time > nextFire) {
 			nextFire = Time.time + fireRate;
-			//GameObject clone = 
 			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+			audio.Play ();
 		}
 	}
 
